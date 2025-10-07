24f6194fdfde2d7792d68ae209311683dce98345
bergenhem <cbergenhem@gmail.com>
2025-10-07 13:14:37 -0400

    Extract client-side turnstile scripts to helper file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,8 @@
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string;
   readonly SUPABASE_ANON_KEY: string;
+  readonly TURNSTILE_SITE_KEY: string;
+  readonly TURNSTILE_SECRET_KEY: string;
 }
 
 interface ImportMeta {
